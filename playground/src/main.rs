b20b18bbc8f16a350702b0bd7221c2004070469d
Rainer Stropek <rainer@software-architects.at>
2026-01-20 16:29:48 +0100

    Traits

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,16 +1,27 @@
 #![allow(dead_code)]
 
-use std::fmt::Debug;
+use std::fmt::Display;
 
+#[derive(Debug, PartialEq, Clone)]
 pub struct ConsultingWork {
     what: String,
     hours: f32,
-    rate: f32
+    rate: f32,
+}
+
+impl Display for ConsultingWork {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}: {} hours at ${}/hour",
+            self.what, self.hours, self.rate
+        )
+    }
 }
 
 pub struct Material {
     description: String,
-    cost: f32
+    cost: f32,
 }
 
 trait Billable {
@@ -49,6 +60,13 @@ fn print_invoice(item: &impl Billable) {
     println!("Invoice amount ${}", item.total());
 }
 
+fn print_billable<T>(item: &T)
+where
+    T: Billable + Display,
+{
+    println!("Invoice amount {} for ${}", item.total(), item);
+}
+
 fn apply_rebate_to_item(item: &mut impl Rebatable) {
     item.apply_rebate();
 }
@@ -74,9 +92,42 @@ impl Billable for () {
 fn create_billable() -> impl Billable {
     Material {
         description: String::from("Gadget"),
-        cost: 99.99
+        cost: 99.99,
     }
 }
+
+fn create_billable_based_on_description(material_desc: Option<&str>, costs: f32) -> Box<dyn Billable> {
+    if let Some(desc) = material_desc {
+        Box::new(Material {
+            description: desc.to_string(),
+            cost: costs,
+        })
+    } else {
+        Box::new(costs)
+    }
+}
+
+fn print_dyn_billable(item: &Box<dyn Billable>) {
+    println!("Invoice amount ${}", item.total());
+}
+
+/*
+abstract class Xyz {
+  virtual something()...
+}
+
+class Abc : Xyz {
+  override something()...
+}
+
+class Def : Xyz {
+  override something()...
+}
+
+Xyz *myObj = ...;
+myObj->something();
+
+*/
 
 fn main() {
     let mut work = 42.0;
@@ -91,7 +142,37 @@ fn main() {
     let consulting = ConsultingWork {
         what: String::from("Development"),
         hours: 10.0,
-        rate: 150.0
+        rate: 150.0,
     };
+    println!("{:?}", consulting);
     print_invoice(&consulting);
+    print_billable(&consulting);
+
+    let consulting2 = ConsultingWork {
+        what: String::from("Development"),
+        hours: 10.0,
+        rate: 150.0,
+    };
+    assert_eq!(consulting, consulting2);
+
+    let billables = vec![
+        create_billable_based_on_description(Some("Widget"), 49.99),
+        create_billable_based_on_description(None, 19.99)
+    ];
+    for b in billables {
+        print_dyn_billable(&b);
+    }
+
+    let billables: Vec<Box<dyn Billable>> = vec![
+        Box::new(ConsultingWork {
+            what: String::from("Design"),
+            hours: 5.0,
+            rate: 200.0,
+        }),
+        Box::new(Material {
+            description: String::from("Component"),
+            cost: 29.99,
+        }),
+        Box::new(15.0)
+    ];
 }
