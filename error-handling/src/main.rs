360a735e0dcce7e4952b7a03a7420f01bfdf24e5
Rainer Stropek <rainer@software-architects.at>
2026-01-21 14:51:56 +0100

    Error handling final checkin

---
+++ error-handling/src/main.rs
@@ -0,0 +1,85 @@
+use anyhow::Result;
+use serde_json::Value;
+use thiserror::Error;
+use tokio::{fs::File, io::AsyncReadExt};
+
+async fn read_and_parse(file_name: &str) -> String {
+    let mut f = File::open(file_name).await.unwrap();
+    let mut buf = Vec::new();
+    f.read_buf(&mut buf).await.unwrap();
+    let s = String::from_utf8(buf).unwrap();
+    let v: Value = serde_json::from_str(&s).unwrap();
+    let connection_string = v.get("connectionString").unwrap();
+    connection_string.to_string()
+}
+
+async fn read_and_parse_2(file_name: &str) -> Result<String, &str> {
+    let mut f = File::open(file_name).await.map_err(|_| "File open error")?;
+    let mut buf = Vec::new();
+    f.read_buf(&mut buf).await.map_err(|_| "File read error")?;
+    let s = String::from_utf8(buf).map_err(|_| "UTF-8 conversion error")?;
+    let v: Value = serde_json::from_str(&s).map_err(|_| "JSON parse error")?;
+    let connection_string = v.get("connectionString");
+    match connection_string {
+        Some(cs) => Ok(cs.to_string()),
+        None => Err("Missing connectionString field"),
+    }
+    //v.get("connectionString")
+    //    .map(|cs| cs.to_string())
+    //    .ok_or("Missing connectionString field")
+}
+
+async fn read_and_parse_3(file_name: &str) -> Result<String> {
+    let mut f = File::open(file_name).await?;
+    let mut buf = Vec::new();
+    f.read_buf(&mut buf).await?;
+    let s = String::from_utf8(buf)?;
+    let v: Value = serde_json::from_str(&s)?;
+    let connection_string = v.get("connectionString");
+    match connection_string {
+        Some(cs) => Ok(cs.to_string()),
+        None => Err(anyhow::anyhow!("Missing connectionString field")),
+    }
+}
+
+#[derive(Error, Debug)]
+enum ConfigurationReaderError {
+    #[error("I/O error (details: {0})")]
+    Io(#[from] std::io::Error),
+    #[error("UTF-8 conversion error")]
+    Utf8(#[from] std::string::FromUtf8Error),
+    #[error("JSON parsing error")]
+    Json(#[from] serde_json::Error),
+    #[error("Unknown error")]
+    Unknown,
+}
+
+async fn read_and_parse_4(file_name: &str) -> Result<String, ConfigurationReaderError> {
+    let mut f = File::open(file_name).await?;
+    let mut buf = Vec::new();
+    f.read_buf(&mut buf).await?;
+    let s = String::from_utf8(buf)?;
+    let v: Value = serde_json::from_str(&s)?;
+    let connection_string = v.get("connectionString");
+    match connection_string {
+        Some(cs) => Ok(cs.to_string()),
+        None => Err(ConfigurationReaderError::Unknown),
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let result = read_and_parse_4("settings.json").await;
+    match result {
+        Ok(connection_string) => println!("Connection string: {}", connection_string),
+        Err(e) => {
+            println!("Error occurred: {}", e);
+            match e {
+                ConfigurationReaderError::Io(e) => println!("I/O error occurred: {}", e),
+                ConfigurationReaderError::Utf8(_) => println!("UTF-8 conversion error occurred"),
+                ConfigurationReaderError::Json(_) => println!("JSON parsing error occurred"),
+                ConfigurationReaderError::Unknown => println!("Unknown error occurred"),
+            }
+        }
+    }
+}
